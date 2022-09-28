cb8e8ba63278654a227bb05308b81f01617f57b3
Sandaime-As <asenov1998@mail.ru>
2022-09-29 02:11:50 +0600

    поправки

--- task16_1/Program.cs
+++ task16_1/Program.cs
@@ -1,16 +1,10 @@
 ﻿// Доп задача: вывести числа в обратном порядке из трехзначного числа
 using static System.Console;
-
 Clear();
 int number = new Random().Next(100,1000);
 WriteLine(number);
-
 int a0 = number%10;
 int a2 = number/100;
 int a1 = (number-(a2*100+a0));
-
-
 int result = a0*100+a1+a2;
-
-//WriteLine($"{a2}{a0}");
 WriteLine(result);
